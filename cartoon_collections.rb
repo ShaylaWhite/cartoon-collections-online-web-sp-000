c3674d433817916cc7150cb9e2edb3e499f5d231
Shayla White <shayladwhite1@gmail.com>
2019-05-26 03:48:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ def long_planeteer_calls(calls)
     end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
+  cheese.include do |cheese|
+    
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
