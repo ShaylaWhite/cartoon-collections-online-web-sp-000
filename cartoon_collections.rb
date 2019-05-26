11a3b473737719502695b1eeb9adc5fd54d4303e
Shayla White <shayladwhite1@gmail.com>
2019-05-26 03:44:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 
-def long_planeteer_calls()
-  # Your code here
+def long_planeteer_calls(calls)
+ long_planeteer_calls.any? do |calls|
+      calls.length > 4
+    end
 end
 
 def find_the_cheese# code an argument here
